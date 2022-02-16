546ab025f5b389882f61721111b3726cf0878fba
Steve Ernstberger <steve32285@gmail.com>
2022-02-16 08:55:56 -0500

    created Etsy card

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import MediaCard from "./components/MediaCard";
+import EtsyMediaCard from "./components/MediaCard/Etsy";
 
 const App = () => {
   return (
@@ -12,18 +12,16 @@ const App = () => {
           Acadia
         </Typography>
         <Typography marginBottom={10}>an idyllic place</Typography>
-        <MediaCard
+        <EtsyMediaCard
           image="https://upload.wikimedia.org/wikipedia/commons/1/1f/Acadia_National_Park%2C_Baker_Island_toward_Mount_Desert_Island.jpg"
-          content={
-            <div>
-              <Typography variant="h6">Acadia National Park</Typography>
-              <Typography>
-                Acadia National Park protects the natural beauty of the highest
-                rocky headlands along the Atlantic coastline of the United
-                States, an abundance of habitats, and a rich cultural heritage.
-              </Typography>
-            </div>
-          }
+          title="Some title goes here"
+          seller="Some seller goes here"
+          price={1234}
+          rating={{
+            average: 4.5,
+            count: 4567,
+          }}
+          freeShipping
         />
       </Box>
     </Container>
---
+++ src/components/MediaCard/Etsy.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import MediaCard from ".";
+import { Chip, Rating, Stack, Typography } from "@mui/material";
+
+interface EtsyMediaCardProps {
+  image: string;
+  title: string;
+  seller: string;
+  rating?: {
+    average: number;
+    count: number;
+  };
+  price: number;
+  freeShipping?: boolean;
+}
+
+const formatMoney = (amount: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  }).format(amount);
+};
+
+const EtsyMediaCard = ({
+  image,
+  title,
+  seller,
+  rating,
+  price,
+  freeShipping = false,
+}: EtsyMediaCardProps) => {
+  return (
+    <MediaCard
+      {...{ image }}
+      content={
+        <div>
+          <Typography component="h3">{title}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {seller}
+          </Typography>
+          {rating && (
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Rating readOnly value={rating.average} precision={0.5} />
+              <Typography
+                variant="body2"
+                component="span"
+                color="text.secondary"
+              >
+                ({rating.count.toLocaleString("en-US")})
+              </Typography>
+            </Stack>
+          )}
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Typography variant="h6">{formatMoney(price)}</Typography>
+            {freeShipping && (
+              <Chip label="FREE shipping" size="small" color="success" />
+            )}
+          </Stack>
+        </div>
+      }
+    />
+  );
+};
+
+export default EtsyMediaCard;
