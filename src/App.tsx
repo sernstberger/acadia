40c2ae5a55f702c907daec7858bd485db4c8f613
Steve Ernstberger <steve32285@gmail.com>
2022-02-15 19:11:34 -0500

    added Acadia definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
-
-export default function App() {
+const App = () => {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with TypeScript
+          Acadia
         </Typography>
-        <ProTip />
-        <Copyright />
+        <Typography>an idyllic place</Typography>
       </Box>
     </Container>
   );
-}
+};
+
+export default App;
