83cbd65fd9c026351b007caad5bd6179e60fecfb
Steve Ernstberger <steve32285@gmail.com>
2022-02-17 15:29:05 -0500

    added analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import ReactGA from "react-ga";
+import App from "./App";
+import theme from "./theme";
+
+ReactGA.initialize("G-EFRHGV9ET5");
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -11,5 +14,5 @@ ReactDOM.render(
     <CssBaseline />
     <App />
   </ThemeProvider>,
-  document.querySelector('#root'),
+  document.querySelector("#root")
 );
