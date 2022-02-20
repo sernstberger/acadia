36107acc2970c919e12b7e8a178db670c1b4b979
Steve Ernstberger <steve32285@gmail.com>
2022-02-20 17:36:22 -0500

    cleanup

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -4,15 +4,12 @@ import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
 import { Button, Stack } from "@mui/material";
 
-const SearchWrapper = styled("div")(({ theme }) => ({
+const SearchWrapper = styled(Stack)(({ theme }) => ({
   border: "1px solid red",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   display: "flex",
-  // flex: "1 0 auto",
-  // "&:hover": {
-  //   backgroundColor: alpha(theme.palette.common.white, 0.25),
-  // },
+
   // marginRight: theme.spacing(2),
   // marginLeft: 0,
   // width: "100%",
@@ -22,47 +19,15 @@ const SearchWrapper = styled("div")(({ theme }) => ({
   // },
 }));
 
-// const SearchIconWrapper = styled("div")(({ theme }) => ({
-//   padding: theme.spacing(0, 2),
-//   height: "100%",
-//   position: "absolute",
-//   pointerEvents: "none",
-//   display: "flex",
-//   alignItems: "center",
-//   justifyContent: "center",
-// }));
-
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   border: "1px solid green",
   flex: 1,
   // color: "inherit",
-  // "& .MuiInputBase-input": {
-  //   padding: theme.spacing(1, 1, 1, 0),
-  //   // vertical padding + font size from searchIcon
-  //   paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-  //   transition: theme.transitions.create("width"),
-  //   width: "100%",
-  //   [theme.breakpoints.up("md")]: {
-  //     width: "20ch",
-  //   },
-  // },
 }));
 
 const Search = () => {
   return (
-    // <SearchWrapper>
-    //   {/* <SearchIconWrapper>
-    //     <SearchIcon />
-    //   </SearchIconWrapper> */}
-    //   <StyledInputBase
-    //     placeholder="Search…"
-    //     inputProps={{ "aria-label": "search" }}
-    //   />
-    //   <Button variant="contained">
-    //     <SearchIcon />
-    //   </Button>
-    // </SearchWrapper>
-    <Stack direction="row">
+    <SearchWrapper direction="row">
       <StyledInputBase
         placeholder="Search…"
         inputProps={{ "aria-label": "search" }}
@@ -70,7 +35,7 @@ const Search = () => {
       <Button variant="contained">
         <SearchIcon />
       </Button>
-    </Stack>
+    </SearchWrapper>
   );
 };
 
